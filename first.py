c8871e6316e75455fc3c79ee9efbf5f550065aab
CQuann <64736557+CQuann@users.noreply.github.com>
2023-10-06 10:18:11 +0300

    Add files via upload
    first task + sounds

---
+++ first.py
@@ -0,0 +1,12 @@
+import struct
+import wave
+
+inp = wave.open('sound.wav', mode='rb')
+out = wave.open('result1.wav', mode='wb')
+amount_frames = inp.getnframes()
+data = struct.unpack("<" + str(amount_frames) + "h", inp.readframes(amount_frames))
+updated_data = data[::-1]
+updated_frames = struct.pack("<" + str(len(updated_data)) + "h", *updated_data)
+out.writeframes(updated_frames)
+inp.close()
+out.close()
