7af4cb6bc7ab38c750aabb08991e31fc866f570c
CQuann <64736557+CQuann@users.noreply.github.com>
2023-10-11 09:10:59 +0300

    Update second.py

--- second.py
+++ second.py
@@ -1,8 +1,8 @@
 import struct
 import wave
 
-inp = wave.open('sound.wav', mode='rb')
-out = wave.open('result1.wav', mode='wb')
+inp = wave.open('in.wav', mode='rb')
+out = wave.open('result2.wav', mode='wb')
 amount_frames = inp.getnframes()
 data = struct.unpack("<" + str(amount_frames) + "h", inp.readframes(amount_frames))
 updated_data = data[::-1]
