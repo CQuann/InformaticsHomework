766fc95008bae602376efb1e85a2a9f43ed30092
CQuann <64736557+CQuann@users.noreply.github.com>
2023-10-11 09:10:33 +0300

    Update first.py

--- first.py
+++ first.py
@@ -1,11 +1,11 @@
 import struct
 import wave
 
-inp = wave.open('sound.wav', mode='rb')
+inp = wave.open('in.wav', mode='rb')
 out = wave.open('result1.wav', mode='wb')
 amount_frames = inp.getnframes()
 data = struct.unpack("<" + str(amount_frames) + "h", inp.readframes(amount_frames))
-updated_data = data[::-1]
+updated_data = data[::2]
 updated_frames = struct.pack("<" + str(len(updated_data)) + "h", *updated_data)
 out.writeframes(updated_frames)
 inp.close()
